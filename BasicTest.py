0f890922597101f336051b9b1319623cbf474afb
TSprech <trey.sprecher+github@gmail.com>
2024-09-17 12:22:38 -0400

    CSV Based Replacement Rules
    Instead of hardcoding the replacements, the code now pulls the replacements from a CSV file and uses that to determine what replacements need to be made. This makes it much easier to customize and expand.

--- BasicTest.py
+++ BasicTest.py
@@ -1,20 +1,33 @@
 import serial
-import time
 import pyautogui
+import csv
 port = serial.Serial(port='COM13', baudrate=115200, timeout=1)
+
+# Convert CSV data to a dictionary
+replacement_dict = {}
+with open('Key.csv') as csv_data:
+    for row in csv.reader(csv_data.readlines()):
+        if len(row) == 2:
+            number, string = row
+            replacement_dict[chr(int(number))] = string
+
+def replace_numbers(input_string: str, replacements: dict):
+    for number, string in replacements.items():
+        input_string = input_string.replace(number, string)
+    return input_string
+
 
 while True:
     try:
-        data = port.readall()
-        if data:
-            print(f"Received: {''.join(' ' + hex(letter)[2:] for letter in data)}")
-            ascii_data = data.decode('ascii')
-            ascii_data = ascii_data.replace('\x04', '{EOT}')
-            ascii_data = ascii_data.replace('\x1E', '{RS}')
-            ascii_data = ascii_data.replace('\x1D', '{GS}')
-            print(ascii_data)
-            pyautogui.write(ascii_data)
-            time.sleep(0.1)
+        new_character = port.readall()
+        if new_character:
+            print(f"Received: {''.join(' ' + hex(letter)[2:] for letter in new_character)}")
+            ascii_character = new_character.decode('ascii')
+            replaced = replace_numbers(ascii_character, replacement_dict)
+            pyautogui.write(replaced)
+            print(replaced)
     except KeyboardInterrupt:
         port.close()
         break
+
+
