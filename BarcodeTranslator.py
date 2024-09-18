a08bb2a82eb0fb8ebf64111e2ecd59a2ce95521a
TSprech <trey.sprecher+github@gmail.com>
2024-09-17 22:37:50 -0400

    Testing Different Printing Strategies
    It would be nice to have a good printing strategy which prints the unprocessed and the processed strings. Currently testing a couple of different methods of doing such. This one prints the old data in red and the new replacements in green.

--- BarcodeTranslator.py
+++ BarcodeTranslator.py
@@ -59,6 +59,29 @@ def replace_numbers(input_string: str, replacements: dict) -> str:
     pattern = re.compile('|'.join(re.escape(key) for key in replacements.keys()))  # Create a regular expression pattern that matches any of the keys
     return pattern.sub(lambda match: replacements[match.group(0)], input_string)  # Use the pattern to replace all occurrences in one pass
 
+def print_replaced_string(input_string: str, replacements: dict) -> str:
+    text = Text()
+    for character in input_string:
+        if character in replacements:
+            text.append(replacements[character], style="green")
+        else:
+            text.append(character, style="white")
+
+    console.print(text)
+
+def print_original_string(input_string: str, replacements: dict) -> str:
+    text = Text()
+    for character in input_string:
+        if character in replacements:
+            text.append('|', style="red")
+            text.append(str(ord(character)), style="red")
+            text.append('|', style="red")
+        else:
+            text.append(character, style="white")
+
+    console.print(text)
+
+
 def format_and_color_bytearray(byte_array, replacements):
     text = Text()
 
@@ -73,6 +96,22 @@ def format_and_color_bytearray(byte_array, replacements):
     console.print(text)
 
 
+def replace_and_color(input_string: str, replacements: dict, hex=False) -> (str, Text, Text):
+    replaced_string = ''
+    original_text = Text()
+    replaced_text = Text()
+    for character in input_string:
+        if character in replacements:
+            replaced_string += replacements[character]
+            original_text.append(f'|{str(ord(character))}|' if not hex else f'|{ord(character):02X}|', style="red")
+            replaced_text.append(replacements[character] if not hex else f'{replacements[character]:02X}', style="green")
+        else:
+            replaced_string += character
+            original_text.append(character if not hex else f'{character:02X}', style="white")
+            replaced_text.append(character if not hex else f'{character:02X}', style="white")
+    return replaced_string, original_text, replaced_text
+
+
 args = parse_arguments() # Parse command line arguments
 port = serial.Serial(port=args.port, baudrate=args.baud, timeout=0.5) # Connect to the scanner
 replacement_dict = generate_replacement_dict(args.key) # Generate the key replacement look up dictionary
@@ -83,10 +122,17 @@ while True:
     try:
         scanner_data = port.readall()
         if scanner_data:
-            format_and_color_bytearray(scanner_data, replacement_dict)
-            replaced = replace_numbers(scanner_data.decode('ascii'), replacement_dict)
+            # format_and_color_bytearray(scanner_data, replacement_dict)
+            # replaced = replace_numbers(scanner_data.decode('ascii'), replacement_dict)
+            replaced, red, green = replace_and_color(scanner_data.decode('ascii'), replacement_dict, hex=True)
             pyautogui.write(replaced)
-            log.info(f'Replaced: {replaced}')
+            console.print(red)
+            console.print(green)
+            # print_original_string(scanner_data.decode('ascii'), replacement_dict)
+            # print_replaced_string(scanner_data.decode('ascii'), replacement_dict)
+            # log.info(f'Replaced: {replaced}')
     except KeyboardInterrupt:
         port.close()
         break
+
+
